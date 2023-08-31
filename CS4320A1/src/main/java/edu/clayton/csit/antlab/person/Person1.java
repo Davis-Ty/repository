4e5a8109f8a169e792c6720c02b79f5cd56858ad
Davis-Ty <tkdavis21@outlook.com>
2023-08-31 18:59:55 -0400

    Person1 Inputs Completed

--- CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person1.java
+++ CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person1.java
@@ -6,18 +6,21 @@ package edu.clayton.csit.antlab.person;
  *  modified string 
  *  
  *  @author Qu
+ *  @author Tyon Davis
  *  @version 1.1
  */
 public class Person1 {
   /** Holds the persons real name */
   private String name;
+  
   	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
+	 * @param now name takes pname place
 	 */
-  public Person1(String pname) {
-    name = pname;
+  public Person1(String name) {
+    this.name = name;
   }
   	/**
 	 * This method should take the string
@@ -29,9 +32,19 @@ public class Person1 {
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
-	private String calc(String input) {
+	private String calc(String name) {
+		String nameRotated ="";
 	  //Person 1 put your implementation here
-	  return null;
+		name.charAt(0);
+		for (int i=2; i<name.length(); i++){
+			nameRotated=nameRotated+name.charAt(i);
+											
+		}
+		//Rotating first two letters to the end
+		for (int i=0; i<2; i++){
+			nameRotated=nameRotated+name.charAt(i);									
+		}
+	  return nameRotated;
 	}
 	
 	/**
@@ -42,8 +55,8 @@ public class Person1 {
 	 * @return the string representing the 
 	 *         object
 	 */
-	public String toString(String input) {
-	  return name + calc(input);
+	public String toString(String nameRotated) {
+	  return name + calc(nameRotated);
 	}
 
 }
